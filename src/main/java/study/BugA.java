dfbf4a616e5a4e0cbdcb4a2d9a796dccac8d91aa
Jandir Medina <>
2026-03-09 23:16:52 +0000

    Add buggy code and tests

---
+++ src/main/java/study/BugA.java
@@ -0,0 +1,8 @@
+package study;
+
+public class BugA {
+    // Your buggy code here
+    public static int yourMethod(int x) {
+        return x * 2;  // Example
+    }
+}
---
+++ src/test/java/study/BugATest.java
@@ -0,0 +1,10 @@
+package study;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BugATest {
+    @Test
+    public void testYourBug() {
+        assertEquals(4, BugA.yourMethod(2));
+    }
+}
